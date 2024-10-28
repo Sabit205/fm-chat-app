2d9ae9676bc285d6fb732437268e8304ab02a8cd
Sabit205 <130175838+Sabit205@users.noreply.github.com>
2024-10-28 22:52:08 +0600

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://fm-chat-app.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
